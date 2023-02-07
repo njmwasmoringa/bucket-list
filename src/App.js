a702721e729b7fc28fb7da07caa0d0ae21d2732c
Julius <njmwas@gmail.com>
2023-02-07 20:03:44 +0300

    React information flow

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import BucketItem from './components/bucket-item/BucketItem';
 import data from './data/bucketitems.json';
+import { useState } from 'react';
+
 function App() {
+
+  const [buckets, setBuckets] = useState( data );
+
+  function bucketLiked( id, newNumberOfLikes ){
+    setBuckets( (currentBucket)=>{
+
+      const newUpdateBuckets = [...currentBucket];
+
+      const theLickedBucketIndex = newUpdateBuckets.findIndex( bucket => bucket.id === id );
+      console.log(newUpdateBuckets[ theLickedBucketIndex ]);
+
+      newUpdateBuckets[ theLickedBucketIndex ].likes = newNumberOfLikes;
+
+      return newUpdateBuckets;
+    } );
+  }
 
   return (
     <div>
@@ -10,12 +28,14 @@ function App() {
       <h1>My Bucketlist Items</h1>
       
       <div className="bucket-container">
-      { data.map( (item)=><BucketItem 
+      { buckets.map( (item, index)=><BucketItem key={item.id}
           title={item.title} 
           description={item.description}
           img={item.img}
           id={item.id}
-        />  ) }
+          likes={item.likes}
+          licked={ bucketLiked } 
+        >{index}</BucketItem>  ) }
       </div>
       <div>Something</div>
 
--- src/components/bucket-item/BucketItem.js
+++ src/components/bucket-item/BucketItem.js
@@ -16,17 +16,33 @@
  * }
  */
 
+import { useState } from 'react';
 import './BucketItem.css';
 
-export default function BucketItem( { title, description, img, id } ){
+export default function BucketItem( { children, title, description, img, id, likes, licked } ){
+
+
+    let [ bucketLikes, setBucketLikes ] = useState( likes );
+    
+    function like(){
+        setBucketLikes( (currentBuckeLIkes) => {
+            const newUpdatedLikes = currentBuckeLIkes + 1;            
+            licked( id, newUpdatedLikes);
+            return newUpdatedLikes;
+        } );
+    }
+    // console.log(count);
 
     return <div className="bucket-item">
-
+        {children}
         <img src={ img } />
         <h3> { title } </h3>
         <p>{ description }</p>
 
+        <button title="click to like" onClick={ like } ><span>{ bucketLikes }</span> Likes <strong>{likes}</strong> </button>
+
         <a href="" title="title">More Details</a>
+
 
     </div>
 
